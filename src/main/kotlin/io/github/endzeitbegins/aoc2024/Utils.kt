4c1323a7fd422eaa03231d625807cf945ccbfa22
EndzeitBegins <16666115+EndzeitBegins@users.noreply.github.com>
2024-12-01 08:25:49 +0100

    feat: add solution for day01

--- src/main/kotlin/io/github/endzeitbegins/aoc2024/Utils.kt
+++ src/main/kotlin/io/github/endzeitbegins/aoc2024/Utils.kt
@@ -4,7 +4,7 @@ import kotlin.io.path.Path
 import kotlin.io.path.readLines
 import kotlin.io.path.readText
 
-fun readInput(name: String) = Path("src/io/github/endzeitbegins/aoc2024/$name").readText()
+fun readInput(name: String) = Path("src/main/kotlin/io/github/endzeitbegins/aoc2024/$name").readText()
 fun readInputLines(name: String) = readInput(name).lines()
 
 fun <T> checkSolution(solution: T, expectedSolution: T) =
--- src/main/kotlin/io/github/endzeitbegins/aoc2024/day01/day01.kt
+++ src/main/kotlin/io/github/endzeitbegins/aoc2024/day01/day01.kt
@@ -1,26 +1,54 @@
 package io.github.endzeitbegins.aoc2024.day01
 
 import io.github.endzeitbegins.aoc2024.checkSolution
-import io.github.endzeitbegins.aoc2024.readInput
+import io.github.endzeitbegins.aoc2024.readInputLines
+import kotlin.math.abs
 
-fun part1(input: String): Int {
-    TODO()
+fun part1(input: List<String>): Int {
+    val (leftList, rightList) = parseLists(input)
+
+    return leftList.sorted()
+        .zip(rightList.sorted())
+        .sumOf { (a, b) -> abs(a - b) }
 }
 
-fun part2(input: String): Int {
-    TODO()
+fun part2(input: List<String>): Int {
+    val (leftList, rightList) = parseLists(input)
+
+    val leftFrequencies = leftList.toFrequencyMap()
+    val rightFrequencies = rightList.toFrequencyMap()
+
+    return leftFrequencies
+        .map { (locationId, occurrences) -> locationId * (rightFrequencies[locationId] ?: 0) * occurrences }
+        .sum()
+}
+
+private fun List<Int>.toFrequencyMap(): Map<Int, Int> =
+    groupingBy { it }
+        .eachCount()
+
+private fun parseLists(input: List<String>): Pair<MutableList<Int>, MutableList<Int>> {
+    val leftList = mutableListOf<Int>()
+    val rightList = mutableListOf<Int>()
+
+    for (line in input) {
+        val (itemA, itemB) = line.split("""\s+""".toRegex(), 2)
+        leftList += itemA.toInt()
+        rightList += itemB.toInt()
+    }
+
+    return Pair(leftList, rightList)
 }
 
 fun main() {
-    val testInput = readInput("day01/test-input.txt")
-//    val testInput2 = readInput("day01/test-input2.txt")
-    val input = readInput("day01/input.txt")
+    val testInput = readInputLines("day01/test-input.txt")
+    val input = readInputLines("day01/input.txt")
 
     // part 1
-    checkSolution(part1(testInput), 142)
+    checkSolution(part1(testInput), 11)
     println(part1(input))
 
     // part 2
-//    checkSolution(part2(testInput2), 281)
-//    println(part2(input))
+    checkSolution(part2(testInput), 31)
+    println(part2(input))
 }
